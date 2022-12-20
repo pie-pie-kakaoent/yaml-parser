80ed895ba0cdf4955f576ac499bae9125d04f8aa
pie.pie <pie.pie@kakaoent.com>
2022-12-20 15:45:25 +0900

    Add test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,29 +1,11 @@
-import {wait} from '../src/wait'
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
+import {flattenFromYamlFile} from '../src/flatten'
 import {expect, test} from '@jest/globals'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
-
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
-
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execFileSync(np, [ip], options).toString())
+test('test', async () => {
+  const flattenObject = await flattenFromYamlFile('test.yaml')
+  expect(flattenObject.workloadIdentityProvider).toEqual('woooo')
+  expect(flattenObject.cloudrun_env_vars_0).toEqual('foo=bar')
+  expect(flattenObject.object_key1).toEqual('value')
+  expect(flattenObject.object_key2_0).toEqual('a')
+  expect(flattenObject.object_key2_1).toEqual(2)
 })
