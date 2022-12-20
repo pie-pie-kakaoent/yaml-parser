0ca5437f7217636dd5eccc1b28720775e95faee3
pie.pie <pie.pie@kakaoent.com>
2022-12-20 15:43:29 +0900

    Add orchestration code

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import * as core from '@actions/core'
+import {flattenFromYamlFile} from './flatten'
 
 async function run(): Promise<void> {
   try {
-    // TODO: fill
+    const filename: string = core.getInput('filename', {required: true})
+    const flattenObject = await flattenFromYamlFile(filename)
+    Object.entries(flattenObject).forEach(([key, value]) =>
+      core.setOutput(key, value)
+    )
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
-
 run()
